5c9c23a6e6dfb967244527fc176ce7c366eb04eb
Felipe Monnerat <femonner@c1r9s3.42lisboa.com>
2022-01-23 16:21:02 +0000

    OK 85/100

---
+++ c00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: femonner <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/13 18:56:15 by femonner          #+#    #+#             */
+/*   Updated: 2022/01/18 13:47:04 by femonner         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	letter;
+
+	letter = 'z';
+	while (letter >= 'a')
+	{
+		write(1, &letter, 1);
+		letter --;
+	}
+}
